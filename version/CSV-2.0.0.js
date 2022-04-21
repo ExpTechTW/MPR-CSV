7b21ee1dc84523d136e76f11d71037e0a681bf92
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-04-21 12:19:47 +0800

    update CSV-2.0.0.js

--- version/CSV-2.0.0.js
+++ version/CSV-2.0.0.js
@@ -1,87 +1,44 @@
 'use strict'
 
-
-
 const Plugin = {
-
     "name": "CSV",
-
-    "version": "1.0.0",
-
+    "version": "2.0.0",
     "depends": {
-
-        "pluginLoader": "2.X.X"
-
+        "pluginLoader": ">=3.0.0"
     },
-
     "Events": [],
-
     "Commands": [],
-
     "author": ["whes1015"],
-
     "link": "https://github.com/ExpTechTW/MPR-CSV",
-
     "resources": ["AGPL-3.0"],
-
     "description": "處理 CSV 檔案"
-
 }
-
-
 
 async function CSVconvert(csv, symbol) {
-
     let data = csv.split("\n")
-
     let Json = []
-
     let cache = {}
-
     for (let index = 0; index < data.length; index++) {
-
         if (index == 0) {
-
             let Data = data[index].split(symbol)
-
             for (let index = 0; index < Data.length; index++) {
-
                 cache[index] = Data[index]
-
                 Json.push({
-
                     "name": Data[index].replace("\r", ""),
-
                     "value": []
-
                 })
-
             }
-
         } else {
-
             let Data = data[index].split(symbol)
-
             for (let index = 0; index < Data.length; index++) {
-
                 Json[index]["value"].push(Data[index].replace("\r", ""))
-
             }
-
         }
-
     }
-
     return Json
-
 }
 
-
-
 module.exports = {
-
     Plugin,
-
     CSVconvert
-
 }
